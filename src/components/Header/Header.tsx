c785c44d37ba044a4c960b79b8e44ea02d1419a5
musk <cpt@cashmallow.com>
2023-01-30 01:22:41 +0900

    ✅ Add: 기본 헤더 컴포넌트 추가

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="m-0 bg-black h-24  font-bold">
+      <div className="text-white">Home</div>
+    </div>
+  );
+}
+
+export default Header;
