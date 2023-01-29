224c433a695448fbe264f8e7b4d8fe215625a291
musk <cpt@cashmallow.com>
2023-01-30 01:17:24 +0900

    ✅ Add: prettier 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  parser: "typescript",
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  tailwindConfig: './tailwind.config.js'
+};
