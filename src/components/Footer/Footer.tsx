02b6b27cc095a920ae3c7b5b6613e39026cbc2c7
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-30 18:29:45 +0900

    ✅ Add: Footer 컴포넌트 생성

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return <div className="m-0 h-24 bg-black  font-bold">Footer</div>;
 }
 
 export default Footer;
