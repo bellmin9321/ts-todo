60ac879e4f7b6b4f5e5741d2f7eeb88c347aa29f
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-30 23:34:39 +0900

    ✨ Feat: Input에 text 입력 시 todo 추가 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useRecoilState } from 'recoil';
+import { todoListState, todoTextState } from '../../recoil';
 
 function Header() {
+  const [todoList, setTodoList] = useRecoilState<string[]>(todoListState);
+  const [text, setText] = useRecoilState<string>(todoTextState);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current !== null) {
+      inputRef.current.focus();
+    }
+  });
+
+  const onChangeText = (e: React.ChangeEvent<HTMLInputElement | null>) => {
+    setText(e.target.value);
+  };
+
+  // KeyboardEvent 참고 https://minjs.tistory.com/2
+  const onAddTodoText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && inputRef.current !== null) {
+      setText('');
+      setTodoList([...todoList, text]);
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div className="m-0 h-24 flex-row content-center justify-center bg-black">
       <div className="text-4xl font-bold text-yellow-300">TODO LIST</div>
@@ -8,6 +34,10 @@ function Header() {
         type="text"
         placeholder=" 할 일을 입력하세요"
         className="text-m mt-5 w-1/3"
+        onChange={onChangeText}
+        onKeyDown={onAddTodoText}
+        value={text}
+        ref={inputRef}
       />
     </div>
   );
