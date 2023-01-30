33b07cfd3de7d71d53f43e5c08d815713b416a03
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-30 18:30:07 +0900

    ✅ Add: Header 컴포넌트 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function Header() {
   return (
-    <div className="m-0 bg-black h-24  font-bold">
-      <div className="text-white">Home</div>
+    <div className="m-0 h-24 flex-row content-center justify-center bg-black">
+      <div className="text-4xl font-bold text-yellow-300">TODO LIST</div>
+      <input
+        type="text"
+        placeholder=" 할 일을 입력하세요"
+        className="text-m mt-5 w-1/3"
+      />
     </div>
   );
 }
