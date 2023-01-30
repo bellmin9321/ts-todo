611b7a36424859d6bbe151a44691ff80498346a0
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-30 23:58:26 +0900

    ✨ Feat: TODO & DONE UI 작업 및 List 삭제 기능 구현

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useRecoilState } from 'recoil';
+import { todoListState } from '../../recoil';
 
 function TodoList() {
-  return <button>TODO List</button>;
+  const [todoList, setTodoList] = useRecoilState<string[]>(todoListState);
+  const [doneList, setDoneList] = useState<string[]>([]);
+
+  const handleDelete = (array: string[], index: number): void => {
+    const filtered = array.filter(value => value !== array[index]);
+    array === todoList
+      ? setTodoList([...filtered])
+      : setDoneList([...filtered]);
+  };
+
+  return (
+    <section className="m-0 flex h-screen flex-row bg-sky-300 font-bold">
+      <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
+        <div className="h-1/8 w-full bg-blue-600 text-white">TODO</div>
+        <div className="my-5 mx-3 flex h-full w-4/5 flex-col justify-center">
+          {todoList.map((todo: string, i: number) => {
+            return (
+              <div key={uuidv4()}>
+                <div className="flex  justify-between ">
+                  <span className="mb-2">
+                    <input type="checkbox" className="mr-2" />
+                    <li className="inline-block hover:cursor-pointer">
+                      {todo}
+                    </li>
+                  </span>
+                  <span>
+                    <button onClick={() => handleDelete(todoList, i)}>
+                      ❌
+                    </button>
+                  </span>
+                </div>
+                {i < todoList.length - 1 && <hr className="mb-2" />}
+              </div>
+            );
+          })}
+        </div>
+      </ul>
+      <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
+        <div className="h-1/8 w-full bg-red-600 text-white">DONE</div>
+        <div className="my-5 mx-3 flex h-full w-4/5 flex-col justify-center">
+          {doneList.map((done: string, i: number) => {
+            return (
+              <div key={uuidv4()}>
+                <div className="flex  justify-between ">
+                  <span className="mb-2">
+                    <li className="inline-block">{done}</li>
+                  </span>
+                  <span>
+                    <button onClick={() => handleDelete(doneList, i)}>
+                      🗑️
+                    </button>
+                  </span>
+                </div>
+                {i < doneList.length - 1 && <hr className="mb-2" />}
+              </div>
+            );
+          })}
+        </div>
+      </ul>
+    </section>
+  );
 }
 
 export default TodoList;
