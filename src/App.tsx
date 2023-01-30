8b078e6400f80b21cd9c45c2160ff29366d3760f
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-30 23:03:10 +0900

    ✅ Add: App.tsx에 recoil 초기 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import TodoList from "./components/TodoList/TodoList";
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <TodoList />
-      <Footer />
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <Header />
+        <TodoList />
+        <Footer />
+      </div>
+    </RecoilRoot>
   );
 }
 
