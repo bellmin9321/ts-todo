ab97231a4a6bb2d0bb84afa6ebf4b6dc83748bd9
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-01 02:21:44 +0900

    ✅ Add: status 키 추가 및 recoil selector 이용한 상태값 추가

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,43 +1,53 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
-import { TodoList } from '../types';
+import { TodoListType } from '../types';
 
-export const list = ['알고리즘', '면접 준비', '프로젝트', '이력서 작성'];
-
-export const objList = [
+export const todoList = [
   {
     id: uuidv4(),
     title: '알고리즘 문제 풀이',
     desc: 'leetcode & 프로그래머스 하루에 각 1문제씩 풀기',
+    status: 'todo',
   },
   {
     id: uuidv4(),
     title: '면접 준비',
     desc: '하루에 기술 면접 2개씩 설명해보기',
+    status: 'todo',
   },
   {
     id: uuidv4(),
     title: '토이 프로젝트 하기',
     desc: 'react, ts, tailwind, recoil, react-query 사용해보기',
+    status: 'todo',
   },
   {
     id: uuidv4(),
     title: '이력서 작성',
     desc: '자기소개, Tech stack, 프로젝트 설명',
+    status: 'todo',
   },
 ];
 
-export const todoListState = atom<string[]>({
+export const listState = atom<TodoListType[]>({
+  key: 'listState',
+  default: todoList,
+});
+
+export const todoListState = selector<TodoListType[]>({
   key: 'todoListState',
-  default: list,
+  get: ({ get }) => {
+    const list = get(listState);
+
+    return list.filter(item => item.status === 'todo');
+  },
 });
 
-export const objListState = atom<TodoList[]>({
-  key: 'objListState',
-  default: objList,
-});
+export const doneListState = selector<TodoListType[]>({
+  key: 'doneListState',
+  get: ({ get }) => {
+    const list = get(listState);
 
-export const todoTextState = atom({
-  key: 'todoTextState',
-  default: '',
+    return list.filter(item => item.status === 'done');
+  },
 });
