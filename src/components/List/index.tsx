97edd99deecdb74aeca1a2c3288399125e04544d
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-01 02:45:01 +0900

    ✨ Feat: 재사용 가능한 List 컴포넌트 추가

---
+++ src/components/List/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ListProps } from '../../types';
+import { Input } from '../Input';
+
+const List = ({
+  state,
+  list,
+  onCheckTodo,
+  onShowTodoDetail,
+  onDeleteTodo,
+}: ListProps) => {
+  return (
+    <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
+      <div className="h-1/8 w-full bg-blue-600 text-white">{state}</div>
+      <div className="my-5 mx-3 flex h-full w-2/3 flex-col justify-center">
+        {list.map((item, i) => {
+          const { id, title, status } = item;
+          return (
+            <div key={id}>
+              <div className="flex justify-between ">
+                <span className="mb-2">
+                  <Input
+                    type="checkbox"
+                    className="mr-2"
+                    onClick={() => onCheckTodo(id, status)}
+                    defaultChecked={state === 'DONE' ?? false}
+                  />
+                  <li
+                    className="inline-block hover:cursor-pointer"
+                    onClick={() => onShowTodoDetail(item)}
+                  >
+                    {title}
+                  </li>
+                </span>
+                <span>
+                  <button onClick={() => onDeleteTodo(i)}>❌</button>
+                </span>
+              </div>
+              {i < list.length - 1 && <hr className="mb-2" />}
+            </div>
+          );
+        })}
+      </div>
+    </ul>
+  );
+};
+
+export default List;
