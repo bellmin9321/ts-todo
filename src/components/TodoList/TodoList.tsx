211219e9e61cf1e5e63c83ba474efd89a7cf87e3
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-01 02:45:16 +0900

    🌳 Refactor: List 컴포넌트룰 이용하여 리팩토링

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -4,6 +4,7 @@ import { doneListState, todoListState } from '../../recoil';
 import { Input } from '../Input';
 import Modal from '../Modal';
 import useTodo from '../../hooks/useTodo';
+import List from '../List';
 
 const TodoList: FC = () => {
   const {
@@ -19,64 +20,20 @@ const TodoList: FC = () => {
 
   return (
     <section className="m-0 flex h-screen flex-row bg-sky-300 font-bold">
-      <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
-        <div className="h-1/8 w-full bg-blue-600 text-white">TODO</div>
-        <div className="my-5 mx-3 flex h-full w-2/3 flex-col justify-center">
-          {todoList.map((todo, i) => {
-            const { id, title, status } = todo;
-            return (
-              <div key={id}>
-                <div className="flex justify-between ">
-                  <span className="mb-2">
-                    <Input
-                      type="checkbox"
-                      className="mr-2"
-                      onClick={() => checkTodo(id, status)}
-                    />
-                    <li
-                      className="inline-block hover:cursor-pointer"
-                      onClick={() => showTodoDetail(todo)}
-                    >
-                      {title}
-                    </li>
-                  </span>
-                  <span>
-                    <button onClick={() => deleteTodo(i)}>❌</button>
-                  </span>
-                </div>
-                {i < todoList.length - 1 && <hr className="mb-2" />}
-              </div>
-            );
-          })}
-        </div>
-      </ul>
-      <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
-        <div className="h-1/8 w-full bg-red-600 text-white">DONE</div>
-        <div className="my-5 mx-3 flex h-full w-4/5 flex-col justify-center">
-          {doneList.map((done, i) => {
-            const { id, title, status } = done;
-            return (
-              <div key={id}>
-                <div className="flex  justify-between ">
-                  <span className="mb-2">
-                    <Input
-                      type="checkbox"
-                      className="mr-2"
-                      onClick={() => checkTodo(id, status)}
-                      defaultChecked={true}
-                    />
-                    <li className="inline-block">{title}</li>
-                  </span>
-                  <span>
-                    <button onClick={() => deleteTodo(i)}>🗑️</button>
-                  </span>
-                </div>
-                {i < doneList.length - 1 && <hr className="mb-2" />}
-              </div>
-            );
-          })}
-        </div>
-      </ul>
+      <List
+        state={'TODO'}
+        list={todoList}
+        onCheckTodo={checkTodo}
+        onShowTodoDetail={showTodoDetail}
+        onDeleteTodo={deleteTodo}
+      />
+      <List
+        state={'DONE'}
+        list={doneList}
+        onCheckTodo={checkTodo}
+        onShowTodoDetail={showTodoDetail}
+        onDeleteTodo={deleteTodo}
+      />
       {isOpenModal && <Modal todo={todo} handleModal={setOpenModal} />}
     </section>
   );
