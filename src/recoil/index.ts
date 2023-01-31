b47cc9d46982c3e5bce244ad0338c9c92b0f430e
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-31 11:25:54 +0900

    🌳 Refactor: constants에 있던 리스트 배열들 recoil로 이동

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,9 +1,40 @@
 import { atom } from 'recoil';
-import { list } from '../constants';
+import { v4 as uuidv4 } from 'uuid';
+import { TodoList } from '../types';
 
-export const todoListState = atom({
+export const list = ['알고리즘', '면접 준비', '프로젝트', '이력서 작성'];
+
+export const objList = [
+  {
+    id: uuidv4(),
+    title: '알고리즘 문제 풀이',
+    desc: 'leetcode & 프로그래머스 하루에 각 1문제씩 풀기',
+  },
+  {
+    id: uuidv4(),
+    title: '면접 준비',
+    desc: '하루에 기술 면접 2개씩 설명해보기',
+  },
+  {
+    id: uuidv4(),
+    title: '토이 프로젝트 하기',
+    desc: 'react, ts, tailwind, recoil, react-query 사용해보기',
+  },
+  {
+    id: uuidv4(),
+    title: '이력서 작성',
+    desc: '자기소개, Tech stack, 프로젝트 설명',
+  },
+];
+
+export const todoListState = atom<string[]>({
   key: 'todoListState',
   default: list,
+});
+
+export const objListState = atom<TodoList[]>({
+  key: 'objListState',
+  default: objList,
 });
 
 export const todoTextState = atom({
