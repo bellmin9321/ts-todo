c54e766bb6486629f155fbc06ebabb3a2c2a413b
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-03 17:37:05 +0900

    ✅ Add: input autoFocus,  name 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,26 +5,30 @@ const InputInner = (
   {
     type,
     placeholder,
+    autoFocus,
     className,
     onChange,
     onKeyDown,
     onClick,
     defaultValue,
     value,
+    name,
     defaultChecked,
   }: InputProps,
-  ref: ForwardedRef<HTMLInputElement>,
+  ref: ForwardedRef<HTMLInputElement | null>,
 ) => {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      autoFocus={autoFocus}
       className={className}
       onChange={onChange}
       onKeyDown={onKeyDown}
       onClick={onClick}
       defaultValue={defaultValue}
       value={value}
+      name={name}
       defaultChecked={defaultChecked}
       ref={ref}
     />
