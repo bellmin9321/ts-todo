cdf5d175affc49143c8a27d3582eabf9e73c87a0
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-03 17:38:54 +0900

    🌳 Refactor: input autoFocus 추가 및 name 으로 상태관리

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 
 const Footer: FC = () => {
-  return <div className="m-0 h-24 bg-black  font-bold">Footer</div>;
+  return (
+    <div className="m-0 flex h-24 items-center justify-center bg-black font-bold text-white">
+      Copyright 2023 bellmin
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,22 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import useInput from '../../hooks/useInput';
 import { Input } from '../Input';
 
 const Header: FC = () => {
   const { onChangeText, text, inputRef, addTodo } = useInput('');
 
-  useEffect(() => {
-    inputRef.current?.focus();
-  }, []);
   return (
     <div className="m-0 h-24 flex-row content-center justify-center bg-black">
-      <div className="text-4xl font-bold text-yellow-300">TODO LIST</div>
+      <div className="py-3 text-4xl font-bold text-yellow-300">TODO LIST</div>
       <Input
         type="text"
         placeholder="할 일을 입력하세요"
-        className="text-m mt-5 w-1/3 pl-2"
+        autoFocus={true}
+        className="text-m w-1/3 pl-2"
         onChange={onChangeText}
         onKeyDown={addTodo}
         value={text}
+        name={'할일'}
         ref={inputRef}
       />
     </div>
