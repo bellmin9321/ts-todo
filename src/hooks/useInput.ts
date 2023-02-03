83068e4409b1fd37a09c92ee2d732cbbbb944986
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-03 17:42:25 +0900

    🌳 Refactor: onChangeText 함수에 switch case로 상태관리

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -7,10 +7,26 @@ import { TodoListType } from '../types';
 const useInput = (initialState: string) => {
   const [todoList, setTodoList] = useRecoilState<TodoListType[]>(listState);
   const [text, setText] = useState<string>(initialState);
+  const [title, setTitle] = useState<string>(initialState);
+  const [desc, setDesc] = useState<string>(initialState);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
+  const onChangeText = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    switch (e.target.name) {
+      case '제목':
+        setTitle(e.target.value);
+        break;
+
+      case '설명':
+        setDesc(e.target.value);
+        break;
+
+      default:
+        setText(e.target.value);
+        break;
+    }
   };
 
   // KeyboardEvent 참고 https://minjs.tistory.com/2
@@ -34,6 +50,10 @@ const useInput = (initialState: string) => {
     onChangeText,
     text,
     setText,
+    title,
+    setTitle,
+    desc,
+    setDesc,
     inputRef,
     addTodo,
   };
