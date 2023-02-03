d5a58fcbffc41bf04cd6671f193c44fdb66758eb
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-03 17:44:36 +0900

    ✨ Feat: input onChange 추가하여 title, desc 상태 관리

--- src/components/TodoDetail.tsx/index.tsx
+++ src/components/TodoDetail.tsx/index.tsx
@@ -1,19 +1,29 @@
+import useInput from '../../hooks/useInput';
 import { TodoDetailProps } from '../../types';
 import { Input } from '../Input';
 
 const TodoDetail = ({ todo }: TodoDetailProps) => {
+  const { onChangeText } = useInput('');
+
   return (
     <>
       <div className="z-10 h-2/3 w-1/3 flex-col justify-center rounded-[20px] border-2 border-black  bg-white">
+        <div className="mt-2">제목</div>
         <Input
+          type="text"
           placeholder="제목"
-          className="my-5 w-4/5 border-2 border-black pl-2"
+          className="mb-2 w-4/5 border-2 border-black pl-2"
+          onChange={onChangeText}
           defaultValue={todo.title}
+          name={'제목'}
         />
-        <Input
-          placeholder="정확히 무엇을 할 지 입력하세요"
-          className="mb-5 h-2/3 w-4/5 border-2 border-black pl-2"
+        <div>설명</div>
+        <textarea
+          placeholder="설명"
+          className="mb-1 h-2/3 w-4/5 resize-none border-2 border-black pl-2"
+          onChange={onChangeText}
           defaultValue={todo.desc}
+          name={'설명'}
         />
         <div>
           <button className="rounded-[7px] bg-blue-500 p-2 text-white">
