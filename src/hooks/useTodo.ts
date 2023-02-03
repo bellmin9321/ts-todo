71498e952c52b0115d7b7b34c1d9c251b6c551c1
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-03 17:47:44 +0900

    ✨ Feat: editTodo 함수 구현 및 modal 관련 상태 recoil로 적용

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -1,17 +1,17 @@
-import { useCallback, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { listState } from '../recoil';
+import { useState } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { listState, modalState } from '../recoil';
 import { TodoListType } from '../types';
 
 const useTodo = () => {
-  const [todoList, setTodoList] = useRecoilState<TodoListType[]>(listState);
+  const [todoList, setTodoList] = useRecoilState(listState);
+  const setOpenModal = useSetRecoilState(modalState);
   const [todo, setTodo] = useState<TodoListType>({
     id: '',
     title: '',
     desc: '',
     status: '',
   });
-  const [isOpenModal, setOpenModal] = useState<boolean>(false);
 
   const deleteTodo = (id: string): void => {
     const tempList = [...todoList];
@@ -19,6 +19,25 @@ const useTodo = () => {
     tempList.splice(targetIndex, 1);
 
     setTodoList([...tempList]);
+  };
+
+  const editTodo = (
+    editedTodo: TodoListType,
+    title: string,
+    desc: string,
+  ): void => {
+    const tempList = [...todoList];
+    const targetIndex = tempList.findIndex(item => item.id === editedTodo.id);
+
+    const tempTarget = {
+      ...editedTodo,
+      title: title ? title : editedTodo.title,
+      desc: desc ? desc : editedTodo.desc,
+    };
+    tempList.splice(targetIndex, 1, tempTarget);
+
+    setTodoList([...tempList]);
+    setOpenModal(false);
   };
 
   const checkTodo = (id: string, state: string): void => {
@@ -29,26 +48,22 @@ const useTodo = () => {
       ...tempList[targetIndex],
       status: state === 'todo' ? 'done' : 'todo',
     };
-    tempList.splice(targetIndex, 1, tempTarget);
+    tempList.splice(targetIndex, 1);
 
-    setTodoList([...tempList]);
+    setTodoList([...tempList, tempTarget]);
   };
 
-  const showTodoDetail = useCallback(
-    (item: TodoListType) => {
-      setTodo(item);
-      setOpenModal(true);
-    },
-    [isOpenModal],
-  );
+  const showTodoDetail = (item: TodoListType) => {
+    setTodo(item);
+    setOpenModal(true);
+  };
 
   return {
     deleteTodo,
+    editTodo,
     checkTodo,
     showTodoDetail,
     todo,
-    isOpenModal,
-    setOpenModal,
   };
 };
 
