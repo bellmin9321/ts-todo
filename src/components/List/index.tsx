cccd7fa3290d1c6ba42aef842325b09efa1411bf
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-09 02:49:44 +0900

    🌳 Refactor: droppable 추가 및 ListItem 컴포넌트로 코드 정리

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,55 +1,31 @@
+import { memo } from 'react';
 import { ListProps } from '../../types';
-import { Input } from '../Input';
+import StrictModeDroppable from './StrictModeDroppable';
 
-const List = ({
-  state,
-  list,
-  onCheckTodo,
-  onShowTodoDetail,
-  onDeleteTodo,
-}: ListProps) => {
+const List = ({ columnId, column, index, children }: ListProps) => {
   return (
-    <ul className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
-      <div
-        className={`h-1/8  + w-full  text-white ${
-          state === 'DONE' ? 'bg-red-600' : 'bg-blue-600'
-        }`}
-      >
-        {state}
-      </div>
-      <div className="my-5 mx-3 flex h-full w-2/3 flex-col justify-center">
-        {list.map((item, i) => {
-          const { id, title, status } = item;
-          return (
-            <div key={id}>
-              <div className="flex justify-between ">
-                <span className="mb-2">
-                  <Input
-                    type="checkbox"
-                    className="mr-2"
-                    onClick={() => onCheckTodo(id, status)}
-                    defaultChecked={state === 'DONE' ?? false}
-                  />
-                  <li
-                    className={`inline-block hover:cursor-pointer ${
-                      state === 'DONE' ? 'line-through' : null
-                    }`}
-                    onClick={() => onShowTodoDetail(item)}
-                  >
-                    {title}
-                  </li>
-                </span>
-                <span>
-                  <button onClick={() => onDeleteTodo(id)}>❌</button>
-                </span>
-              </div>
-              {i < list.length - 1 && <hr className="mb-2" />}
-            </div>
-          );
-        })}
-      </div>
-    </ul>
+    <StrictModeDroppable droppableId={columnId} key={index}>
+      {provided => (
+        <div className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
+          <div
+            className={`h-1/8 w-full  text-white ${
+              column.status === 'done' ? 'bg-red-600' : 'bg-blue-600'
+            }`}
+          >
+            {column.status.toUpperCase()}
+          </div>
+          <ul
+            className="ul"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {children}
+            {provided.placeholder}
+          </ul>
+        </div>
+      )}
+    </StrictModeDroppable>
   );
 };
 
-export default List;
+export default memo(List);
