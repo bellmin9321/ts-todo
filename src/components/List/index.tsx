b138d7bc9dadea1c50502300e0144d793f34c7f6
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-09 03:14:39 +0900

    ✨ Feat: 상태에 따라 제목 색깔 변경하는 함수 구현

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -3,14 +3,25 @@ import { ListProps } from '../../types';
 import StrictModeDroppable from './StrictModeDroppable';
 
 const List = ({ columnId, column, index, children }: ListProps) => {
+  const getTitleColor = (status: string) => {
+    switch (status) {
+      case 'in-progress':
+        return 'bg-green-600';
+      case 'done':
+        return 'bg-red-600';
+      default:
+        return 'bg-blue-600';
+    }
+  };
+
   return (
     <StrictModeDroppable droppableId={columnId} key={index}>
       {provided => (
         <div className="flex h-2/3 w-1/2 flex-col items-center justify-center border-r border-inherit bg-white">
           <div
-            className={`h-1/8 w-full  text-white ${
-              column.status === 'done' ? 'bg-red-600' : 'bg-blue-600'
-            }`}
+            className={`h-1/8 w-full  text-white ${getTitleColor(
+              column.status,
+            )}`}
           >
             {column.status.toUpperCase()}
           </div>
