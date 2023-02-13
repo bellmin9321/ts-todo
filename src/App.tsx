5effdb6d5b2ad633c39e79bc7cfe08635f68531e
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-13 22:37:29 +0900

    🚚 Rename: 컴포넌트 이름 index.tsx로 변경에 따라 import 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { FC } from 'react';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Layout from './components/Layout';
-import TodoList from './components/TodoList/TodoList';
+import TodoList from './components/TodoList';
 
 const App: FC = () => {
   const queryClient = new QueryClient();
